b1823458dd15cc0f777a5db6c4f10a308288ca61
Ryan Ridden-Harper <rridden@ryanr.local.stsci.edu>
2021-03-05 12:32:34 +1300

    changed function to take columns from either size of strap region. Parallel has also been added

--- rescale_straps.py
+++ rescale_straps.py
@@ -7,7 +7,12 @@ from scipy.signal import savgol_filter
 from astropy.stats import sigma_clip
 from astropy.io import fits
 import argparse
+import multiprocessing
+from joblib import Parallel, delayed
 
+# turn off runtime warnings (lots from logic on nans)
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning) 
 
 def grad_clip(data,box_size=100):
     """
@@ -59,6 +64,7 @@ def fit_strap(data):
     y = data.copy()
     y[~grad_clip(y)] = np.nan
     finite = np.isfinite(y)
+    p =np.ones_like(x) * np.nan
     if len(y[finite]) > 5:
         finite = np.isfinite(y)
         #y = median_clipping(y)
@@ -71,10 +77,13 @@ def fit_strap(data):
     return p
 
 
-def Make_fits(data, name, header):
+def Make_fits(data, name, header,integer = True):
     #print('makefits shape ',data.shape)
     newhdu = fits.PrimaryHDU(data, header = header)
-    newhdu.scale('int16', bscale=1.0,bzero=32768.0)
+    if integer:
+        newhdu.scale('int16', bscale=1.0,bzero=32768.0)
+    else:
+        newhdu.scale('float64')
     newhdu.writeto(name,overwrite=True)
     return 
 
@@ -101,16 +110,16 @@ def Rescale_straps(ref_file,mask_file,output,av_size=5):
     sind = np.where(np.nansum((mask & 4),axis=0)>0)[0]
     normals = np.where(np.nansum((mask & 4),axis=0)==0)[0]
     
-    QE = np.zeros_like(mask) * 1.
+    QE = np.ones_like(mask) * 1.
     corrected = deepcopy(data)
     for i in sind:
         closest = np.nanmin(abs(normals-i))
         nind = normals[abs(normals-i) < closest+av_size]
         s1 = fit_strap(data[:,i])
         n1 = fit_strap(np.nanmedian(data[:,nind],axis=1))
-        factor = np.nanmedian(s1/n1)
+        factor = np.nanmedian(n1/s1)
         QE[:,i] = factor
-        corrected[:,i] =  corrected[:,i] / factor
+        corrected[:,i] =  corrected[:,i] * factor
     
     cor_header = deepcopy(ref.header)
     cor_header['STRAPCAL'] = ('T', 'T if the strap QE has been rescaled')
@@ -120,6 +129,57 @@ def Rescale_straps(ref_file,mask_file,output,av_size=5):
     Make_fits(QE*1000, name, cor_header) # scaled to make it work in integer form 
 
     return 'Rescaled {}, saved as {}'.format(ref_file,output)
+
+from copy import deepcopy
+
+def calc_strap_factor(i,breaks,size,normals,data):
+    qe = np.ones_like(data) * 1. * np.nan
+    b = breaks[i]
+    nind = normals[b-av_size:b]
+    eind = normals[b:b+av_size]
+    nind = np.append(nind,eind) + 1
+    norm = fit_strap(np.nanmedian(data[:,nind],axis=1))
+    for j in range(size[i]):  
+        s1 = fit_strap(data[:,normals[b]+1+j])
+        factor = np.nanmedian(norm/s1)
+        qe[:,normals[b]+1+j] = factor
+    return qe
+
+def correct_straps(ref_file,mask_file,output,size=5,parallel=True):
+    ref = fits.open(ref_file)[0]
+    data = ref.data * 1.
+    mask = fits.open(mask_file)[0].data
+
+    sind = np.where(np.nansum((mask & 4),axis=0)>0)[0]
+    normals = np.where(np.nansum((mask & 4),axis=0)==0)[0]
+    
+    breaks = np.where(np.diff(normals,append=0)>1)[0]
+    size = (np.diff(normals,append=0))[np.diff(normals,append=0)>1]
+
+    if parallel:
+        num_cores = multiprocessing.cpu_count()
+        x = np.arange(0,len(breaks))
+        qe = np.array(Parallel(n_jobs=num_cores)(delayed(calc_strap_factor)(i,breaks,size,normals,data) for i in x))
+        qe = np.nanmedian(qe,axis=0)
+        qe[np.isnan(qe)] = 1   
+    else:
+        qe = []
+        for i in range(len(breaks)):
+            qe += [calc_strap_factor(i,breaks,size,normals,data)]
+            qe = np.array(qe)
+            qe = np.nanmedian(qe,axis=0)
+        qe[np.isnan(qe)] = 1   
+
+    cor_header = deepcopy(ref.header)
+    cor_header['STRAPCAL'] = ('T', 'T if the strap QE has been rescaled')
+
+    Make_fits(data * qe, output, cor_header)
+    name = output.split('.fits')[0] + '.qe.fits'
+    Make_fits(qe, name, cor_header,integer=False) # scaled to make it work in integer form 
+
+    return 'Rescaled {}, saved as {}'.format(ref_file,output)
+
+
 
 def define_options(parser=None, usage=None, conflict_handler='resolve'):
     if parser is None:
@@ -133,6 +193,8 @@ def define_options(parser=None, usage=None, conflict_handler='resolve'):
             help=('Full output path/name for the rescaled image'))
     parser.add_argument('--av_size',default = 5,
             help=('number of nearby columns to average.'))
+    parser.add_argument('--parallel',default = True,
+            help=('use parallel processing.'))
 
     return parser
 
@@ -146,6 +208,7 @@ if __name__ == '__main__':
     mask    = args.mask
     save    = args.output
     av_size = int(args.av_size)
+    par     = args.parallel
 
-    Rescale_straps(file, mask, save, av_size)
-    print('Made mask for {}, saved as {}'.format(file,save))
+    correct_straps(file, mask, save, av_size,par)
+    print('Rescaled {}, saved as {}'.format(file,save))
